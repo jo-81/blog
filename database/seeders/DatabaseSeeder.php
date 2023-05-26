0206ba5cd9b1beeb2d8d16c0e243a9cff5293de0
GeoffroyCOL <geoffroy.colpart81@gmail.com>
2023-05-26 17:55:24 +0200

    login (#18) - ajout de laravel breeze et creation d'un utilisateur

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'admin',
+            'role' => 'admin',
+            'email' => 'admin@domaine.fr',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$/z8tfCto31hIqqXbX9AhHeQd8Bz/fLSSHaMtqODgq5uZNMegJJ9la', // 0000
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
---
+++ lang/fr/login.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'email' => 'Adresse email',
+    'password' => 'Mot de passe',
+    'remember-me' => 'Se souvenir de moi',
+];

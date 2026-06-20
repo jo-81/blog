065da4fc01a1e67a828e9ed92d50654e3991e655
jo-81 <geoffroy.colpart81@gmail.com>
2026-06-20 15:37:09 +0200

    feat(#7): modification du fichier index.php pour ajouter la classe ErrorHandler et le package vlucas/phpdotenv

--- public/index.php
+++ public/index.php
@@ -1,9 +1,18 @@
 <?php
 
+use Dotenv\Dotenv;
 use Framework\Http\Kernel;
+use Framework\Debug\ErrorHandler;
 use Framework\Http\ResponseEmitter;
 
 require_once dirname(__DIR__) . "/vendor/autoload.php";
+
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->safeLoad();
+
+$isDebug = filter_var($_ENV['APP_DEBUG'] ?? false, FILTER_VALIDATE_BOOLEAN);
+
+ErrorHandler::register($isDebug);
 
 $container = require_once dirname(__DIR__) . "/config/container.php";
 

9c91a39170e3e596e1053b7d0d54f564a81092da
jo-81 <geoffroy.colpart81@gmail.com>
2026-06-24 15:05:03 +0200

    feature(#15): Suppresion du ErrorHandlingMiddleware et ajout du package middelwares/error-handler close #15

--- config/middlewares.php
+++ config/middlewares.php
@@ -3,28 +3,22 @@
 use Middlewares\Whoops;
 use Middlewares\Debugbar;
 use Middlewares\AccessLog;
+use Middlewares\ErrorHandler;
 use Middlewares\TrailingSlash;
-use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Framework\Http\Middlewares\RoutingMiddleware;
-use Framework\Http\Middlewares\ErrorHandlingMiddleware;
 use Framework\Http\Middlewares\RequestHandlerMiddleware;
 
 return [
     'app.middlewares' => [
         Whoops::class,
-        ErrorHandlingMiddleware::class,
+        ErrorHandler::class,
         Debugbar::class,
         AccessLog::class,
         TrailingSlash::class,
         RoutingMiddleware::class,
         RequestHandlerMiddleware::class,
     ],
-
-    ErrorHandlingMiddleware::class => fn(ContainerInterface $c) => new ErrorHandlingMiddleware(
-        $c->get(ResponseFactoryInterface::class),
-        $c->get('settings.debug')
-    ),
 
     TrailingSlash::class => DI\autowire()->method('redirect', DI\get(ResponseFactoryInterface::class)),
 ];

10783f57c537e972af7ba27c9dcef52ed28c9b4c
jo-81 <geoffroy.colpart81@gmail.com>
2026-06-24 14:58:42 +0200

    feature(#15): Suppresion du MessageTestMiddleware

--- framework/Factories/LoggerFactory.php
+++ framework/Factories/LoggerFactory.php
@@ -1,21 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Framework\Factories;
 
 use Monolog\Logger;
+use Psr\Log\LoggerInterface;
 use Monolog\Handler\StreamHandler;
 use Psr\Container\ContainerInterface;
-use Psr\Log\LoggerInterface;
 
 class LoggerFactory
 {
     public function __invoke(ContainerInterface $container): LoggerInterface
     {
         $logger = new Logger('app');
-        
+
         $logPath = __DIR__ . '/../../var/logs/access.log';
         $logger->pushHandler(new StreamHandler($logPath, \Monolog\Level::Debug));
-        
+
         return $logger;
     }
 }

89d5f8100e559d38983bf4aba9f57b36cdd9a028
jo-81 <geoffroy.colpart81@gmail.com>
2026-07-11 15:05:59 +0200

    feature/23-dahsboard: ajout de l'extension twig UserExtension

--- framework/Http/Exception/ForbiddenException.php
+++ framework/Http/Exception/ForbiddenException.php
@@ -1,10 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Framework\Http\Exception;
 
-class ForbiddenException extends HttpException 
+class ForbiddenException extends HttpException
 {
     protected $message = "Vous n'avez pas les droits nécessaires pour accéder à cette ressource.";
-    
-    public function getStatusCode(): int { return 403; }
+
+    public function getStatusCode(): int
+    {
+        return 403;
+    }
 }
--- framework/Http/Middlewares/AuthentificationMiddleware.php
+++ framework/Http/Middlewares/AuthentificationMiddleware.php
@@ -1,45 +1,46 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Framework\Http\Middlewares;
 
-use Framework\Http\Exception\ForbiddenException;
-use Framework\Http\Exception\UnauthorizedException;
-use Framework\Security\Auth\Authentication;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
+use Framework\Security\Auth\Authentication;
+use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Framework\Http\Exception\ForbiddenException;
+use Framework\Http\Exception\UnauthorizedException;
 
 class AuthentificationMiddleware implements MiddlewareInterface
 {
-    public function __construct(private Authentication $auth, private ContainerInterface $container)
-    {}
+    public function __construct(private Authentication $auth, private ContainerInterface $container) {}
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $accessRoutes = [];
         // Si la clé access_control exist
-        if($this->container->has('access_control')) {
-            if(!is_array($this->container->get('access_control'))) {
+        if ($this->container->has('access_control')) {
+            if (!is_array($this->container->get('access_control'))) {
                 throw new \RuntimeException("La clé 'access_control' doit retourner un array.");
             }
 
-            $accessRoutes = $this->container->get('access_control'); 
+            $accessRoutes = $this->container->get('access_control');
         }
 
         if (empty($accessRoutes)) {
-            return $handler->handle($request);    
+            return $handler->handle($request);
         }
 
         $user = $this->auth->getUser();
-        foreach($accessRoutes as $path => $roles) {
-            if (preg_match("#". $path ."#", $request->getUri()->getPath())) {
+        foreach ($accessRoutes as $path => $roles) {
+            if (preg_match('#' . $path . '#', $request->getUri()->getPath())) {
                 if (is_null($user)) {
                     throw new UnauthorizedException();
                 }
 
-                if (! in_array($user->getRole()->value, $roles)) {
+                if (! in_array($user->getRole()->value, $roles, true)) {
                     throw new ForbiddenException();
                 }
             }
